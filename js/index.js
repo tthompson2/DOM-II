4823a2dd3afd85baa06d1ee9a29f263b2f61683e
Trevor Thompson <trevthom18@gmail.com>
2019-08-09 21:30:59 -0400

    Starting manipulating the DOM elements on the page

--- js/index.js
+++ js/index.js
@@ -1,1 +1,51 @@
 // Your code goes here
+
+var mouseEvent = document.addEventListener('mouseover', () => {
+
+    let aTag = document.querySelectorAll('a');
+    aTag[0].style.color = 'green';
+
+});
+
+var keyDown = document.addEventListener('keydown', (event) => {
+
+    if (event.keyCode === 65)
+    {
+        alert("The 'A'|'a' key is being pressed");
+    }
+});
+
+var wheelEvent = document.addEventListener('wheel', () => {
+
+    alert("You're scrolling your mouse wheel");
+});
+
+
+var dropEvent = document.addEventListener('drop', () => {
+
+    
+});
+
+var loadEvent = document.addEventListener('load', (event) => {
+
+     
+});
+
+
+var resizeEvent = document.addEventListener('resize', () => {
+
+});
+
+var scrollEvent = document.addEventListener('scroll', () => {
+
+
+
+});
+
+var selectEvent = document.addEventListener('select', () = {
+
+});
+
+var dbClickEvent = document.addEventListener('dbClick', () => {
+
+});
